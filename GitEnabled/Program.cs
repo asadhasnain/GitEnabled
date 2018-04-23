9de79f06344f79617c7df9dc0eb7da44824983d0
asadhasnain <asadhasnain@gmail.com>
2018-04-23 14:08:49 +0500

    Commit in Branch FeatureA

--- GitEnabled/Program.cs
+++ GitEnabled/Program.cs
@@ -10,7 +10,7 @@ namespace GitEnabled
         {
         static void Main(string[] args)
             {
-            Console.WriteLine ("Hello World!");
+            Console.WriteLine ("Hello World!-Feature A");
             }
         }
     }
